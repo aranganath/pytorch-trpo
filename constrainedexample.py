143fbadf1d5737bb3ebd5cdca2d5f8ad4daf23e2
aranganath <aditya.ranganath1@gmail.com>
2022-05-10 14:25:04 -0700

    Finally working. But with some bugs ?

--- constrainedexample.py
+++ constrainedexample.py
@@ -6,21 +6,21 @@ import numpy as np
 import torch
 
 
-delta = 20.
-mu = 20.
+delta = 2
+mu = 1e-2
 
 
-#Define the function
-torchrosenbrock = lambda x: torch.sum((1. - x[0])**2 + (x[1] - x[0]**2)**2) 
-torchradconstraint =  lambda x: x[2]*(1-(x[0]**2 + x[1]**2) - x[3])
-torchbarrier = lambda x: 10.0*torch.log(x[3])
-torchfunc = lambda x: torchrosenbrock(x) + torchradconstraint(x) - torchbarrier(x)
-
+#Numpy
 nprosenbrock = lambda x: np.sum((1. - x[0])**2 + (x[1] - x[0]**2)**2)
 npradconstraint =  lambda x: x[2]*(delta-(x[0]**2 + x[1]**2) - x[3])
 npbarrier = lambda x: mu*np.log(x[3])
 npfunc = lambda x: nprosenbrock(x) + npradconstraint(x) - nplogbarrier(x)
 
+#Torch
+torchrosenbrock = lambda x: torch.sum((1. - x[0])**2 + (x[1] - x[0]**2)**2) 
+torchradconstraint =  lambda x: x[2]*(delta**2-(x[0]**2 + x[1]**2) - x[3])
+torchbarrier = lambda x: mu*torch.log(x[3])
+torchfunc = lambda x: torchrosenbrock(x) + torchradconstraint(x) - torchbarrier(x)
 
 #Numpy version of the Hessian of the objective with respect to the parameters
 def npN(x):
@@ -34,28 +34,43 @@ def npN(x):
 	sol = - np.linalg.inv(N) @ grads
 	return sol
 
-
-# x[2]: lamb
-# x[3]: w
 def torchN(x):
 	N = torch.autograd.functional.hessian(torchfunc, x)
 	grads = torch.autograd.grad(torchfunc(x), x, retain_graph=True)[0]
-	return N, grads
+	sol = - torch.linalg.inv(N) @ grads
+	return sol
 
 
-def linesearch(x, sol, func):
+def numpylinesearch(x, sol, func):
 	# Find the solution to the problem
 	# Check if the linesearch sufficicently reduces the funciton
 	# Take the step if it does
 	merit = lambda x: nprosenbrock(x) - npbarrier(x)
-	alpha = 0.9
-	max_iters = 10
+	alpha = 0.9999
+	max_iters = 20
 	i=0
 	while merit(x+sol) >= merit(x) and i < max_iters:
-		from pdb import set_trace
-		set_trace()
 		sol*=alpha
-		print(merit(x+sol))
+		i+=1
+
+	if i < max_iters:
+		return x + sol
+	else:
+		return x
+
+def torchlinesearch(x, sol, func):
+	# Find the solution to the problem
+	# Check if the linesearch sufficicently reduces the funciton
+	# Take the step if it does
+	# merit = lambda x: torchrosenbrock(x) - torchbarrier(x)
+	merit = lambda x: torchfunc(x)
+	alpha = 0.9999
+	max_iters = 20
+	i=0
+	from pdb import set_trace
+	set_trace()
+	while merit(x+sol) >= merit(x) and i < max_iters:
+		sol*=alpha
 		i+=1
 
 	if i < max_iters:
@@ -64,15 +79,17 @@ def linesearch(x, sol, func):
 		return x
 
 
-# torchN, torchgrads = torchN(torch.tensor([10.0,10.0, 2.0,4.0], requires_grad=True))
-# npN, npgrads = defineN()
-
-# torchsol = -torch.inverse(torchN) @ torchgrads
-# npsol = -np.linalg.inv(npN) @ npgrads
-
-start = np.array([0.0,0.0,2.0,1.])
-
+# start = np.array([0.0,0.0,2.0,1.0])
+start = torch.tensor([0.,0.,1e-2,1e2], requires_grad=True)
+print(start.data)
+alpha = 1.2
 for i in range(10):
-	sol = npN(start)
-	print(start)
-	start = linesearch(start, sol, npfunc)
+	sol = torchN(start)
+	start = torchlinesearch(start, sol, npfunc)
+	mu *= alpha
+	print(start.data)
+	print('mu:', mu)
+	print('functional value:', torchfunc(start))
+
+# Trash
+
