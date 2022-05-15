a1c03f0f23bef2976494e8f529dd83db8480bd3d
aranganath <aditya.ranganath1@gmail.com>
2022-05-15 15:07:16 -0700

    Adding visualization tools

--- constrainedexample.py
+++ constrainedexample.py
@@ -4,93 +4,84 @@
 # been computed exactly
 import numpy as np
 import torch
+import pickle as pkl
 
 
-delta = 3
-mu = 1e-8
+delta = 10**3
+mu = 1e-10
 
+# Torch
+# define the rosenbrock function
+torchrosenbrock = lambda x: torch.sum((torch.ones(x[0].shape) - x[0])**2 + (x[1] - x[0]**2)**2)
 
-#Numpy
-nprosenbrock = lambda x: np.sum((1. - x[0])**2 + (x[1] - x[0]**2)**2)
-npradconstraint =  lambda x: x[2]*(delta-(x[0]**2 + x[1]**2) - x[3])
-npbarrier = lambda x: mu*np.log(x[3])
-npfunc = lambda x: nprosenbrock(x) + npradconstraint(x) - nplogbarrier(x)
-
-#Torch
-torchrosenbrock = lambda x: torch.sum((1. - x[0])**2 + (x[1] - x[0]**2)**2)
+# Define the constraint
 torchradconstraint =  lambda x: x[2]*(delta**2-(x[0]**2 + x[1]**2) - x[3])
+
+# Define the log-barrier function
 torchbarrier = lambda x: mu*torch.log(x[3])
+
+#Define the optimization problem
 torchfunc = lambda x: torchrosenbrock(x) + torchradconstraint(x) - torchbarrier(x)
 
-#Numpy version of the Hessian of the objective with respect to the parameters
-def npN(x):
-	grads = np.array([-2*(1-x[0]) + 2*(x[1]-x[0]**2) * (-2*x[0]) + x[2]*(-2*x[0]) , 2*(x[1] - x[0]**2) + x[2]*(-2*x[1])]).T
-	grads = np.append(arr=grads,values=4 - x[0]**2 - x[1]**2 - x[3])
-	grads = np.append(arr=grads, values=-x[2] - 10.0/x[3])
-	N = np.array([[2 - 4*x[1] + 12*x[0]**2-2*x[2], -4*x[0], -2*x[0], 0],
-		[-4*x[0], 2-2*x[2], -2*x[1], 0],
-		[-2*x[0], -2*x[1], 0, -1],
-		[0, 0, -1, 10.0/x[3]**2]])
-	sol = - np.linalg.inv(N) @ grads
-	return sol
-
-def torchN(x):
+def torchsolution(x):
 	N = torch.autograd.functional.hessian(torchfunc, x)
 	grads = torch.autograd.grad(torchfunc(x), x, retain_graph=True)[0]
 	sol = - torch.linalg.inv(N) @ grads
 	return sol
 
 
-def numpylinesearch(x, sol, func):
-	# Find the solution to the problem
-	# Check if the linesearch sufficicently reduces the funciton
-	# Take the step if it does
-	merit = lambda x: nprosenbrock(x) - npbarrier(x)
-	alpha =1
-	max_iters = 20
-	i=0
-	while merit(x+sol) >= merit(x) and i < max_iters:
-		sol*=alpha
-		i+=1
-
-	if i < max_iters:
-		return x + sol
-	else:
-		return x
-
 def torchlinesearch(x, sol, func):
 	# Find the solution to the problem
-	# Check if the linesearch sufficicently reduces the funciton
+	# Check if the linesearch sufficiently reduces the function
 	# Take the step if it does
 	# merit = lambda x: torchrosenbrock(x) - torchbarrier(x)
 	merit = lambda x: torchfunc(x)
-	max_iters = 20
+	max_iters = 10
 	i=0
-	from pdb import set_trace
-	set_trace()
-	while merit(x+sol) >= merit(x) and i < max_iters:
-		global mu, alpha
-		sol*=alpha
-		mu*=alpha2
+	alpha = 1
+	grad = torch.autograd.grad(torchfunc(x), inputs=x)[0]
+	gradsol = torch.autograd.grad(torchfunc(x+alpha*sol), inputs=x)[0]
+	c1 = 0.5
+	c2 = 0.5
+	# from pdb import set_trace
+	# set_trace()
+	while i < max_iters and torchfunc(x+ alpha*sol)<= torchfunc(x) + c1*alpha*grad.dot(sol) and grad.dot(sol)>=c2*grad.dot(sol):
+		alpha = 0.9*alpha
 		i+=1
 
 	if i < max_iters:
-		return x + sol
+		print('Used line search')
+		return x + alpha*sol
 	else:
 		return x
 
 
-# start = np.array([0.0,0.0,2.0,1.0])
-start = torch.tensor([0.,0.,1e-2,1e2], requires_grad=True)
-print(start.data)
-alpha = 0.9
-alpha2 = 1
+class optimizer(object):
+	"""This class will optimizer any objective function provided to it
+	Parameters:
+
+
+	"""
+	def __init__(self, arg):
+		super(optimizer, self).__init__()
+		self.arg = arg
+		
+
+
+iterates = []
+start = torch.tensor([3.,3.,1e-2,1], requires_grad=True)
+print(start.data.numpy())
+iterates.append(start.data.numpy())
 for i in range(10):
-	sol = torchN(start)
-	start = torchlinesearch(start, sol, npfunc)
+	sol = torchsolution(start)
+	start = torchlinesearch(start, sol, torchfunc)
+	iterates.append(start.data.numpy())
 	print(start.data)
 	print('mu:', mu)
 	print('functional value:', torchfunc(start).data)
 
-# Trash
+with open('./rosenbrockresults/raw/path.pkl', 'wb') as file:
+	print('Results saved to ./rosenbrockresults/raw/path.pkl')
+	pkl.dump(iterates, file, protocol=pkl.HIGHEST_PROTOCOL)
+
 
