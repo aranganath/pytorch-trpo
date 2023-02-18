bef11d8d2f226fe9b232a3c822a85e4696dff180
ANRC2020 <siddiquiabbas22@gmail.com>
2023-02-17 16:56:10 -0800

    100 memory and 100 max iterations

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
 from itertools import count
 
-
 import gym
 import scipy.optimize
 
@@ -21,33 +20,23 @@ import os
 from sys import stdout
 import time
 import matplotlib.pyplot as plt
+from pdb import set_trace
 
 torch.set_default_tensor_type('torch.cuda.DoubleTensor')
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 parser = argparse.ArgumentParser(description='PyTorch actor-critic example')
-parser.add_argument('--gamma', type=float, default=0.995, metavar='G',
-                    help='discount factor (default: 0.995)')
-parser.add_argument('--env-name', default="Reacher-v2", metavar='G',
-                    help='name of the environment to run')
-parser.add_argument('--tau', type=float, default=0.97, metavar='G',
-                    help='gae (default: 0.97)')
-parser.add_argument('--l2-reg', type=float, default=1e-3, metavar='G',
-                    help='l2 regularization regression (default: 1e-3)')
-parser.add_argument('--max-kl', type=float, default=1e-2, metavar='G',
-                    help='max kl value (default: 1e-2)')
-parser.add_argument('--damping', type=float, default=1e-1, metavar='G',
-                    help='damping (default: 1e-1)')
-parser.add_argument('--seed', type=int, default=543, metavar='N',
-                    help='random seed (default: 1)')
-parser.add_argument('--batch-size', type=int, default=15000, metavar='N',
-                    help='random seed (default: 1)')
-parser.add_argument('--render', action='store_true',
-                    help='render the environment')
-parser.add_argument('--log-interval', type=int, default=1, metavar='N',
-                    help='interval between training status logs (default: 10)')
+parser.add_argument('--gamma', type=float, default=0.995, metavar='G',help='discount factor (default: 0.995)')
+parser.add_argument('--env-name', default="Reacher-v2", metavar='G',help='name of the environment to run')
+parser.add_argument('--tau', type=float, default=0.97, metavar='G', help='gae (default: 0.97)')
+parser.add_argument('--l2-reg', type=float, default=1e-3, metavar='G', help='l2 regularization regression (default: 1e-3)')
+parser.add_argument('--max-kl', type=float, default=1e-2, metavar='G', help='max kl value (default: 1e-2)')
+parser.add_argument('--damping', type=float, default=1e-1, metavar='G', help='damping (default: 1e-1)')
+parser.add_argument('--seed', type=int, default=543, metavar='N', help='random seed (default: 1)')
+parser.add_argument('--batch-size', type=int, default=15000, metavar='N', help='random seed (default: 1)')
+parser.add_argument('--render', action='store_true', help='render the environment')
+parser.add_argument('--log-interval', type=int, default=1, metavar='N', help='interval between training status logs (default: 10)')
 args = parser.parse_args()
-
 
 def select_action(state, policy_net, value_net):
     state = torch.from_numpy(state).unsqueeze(0)
@@ -59,7 +48,7 @@ def update_params(batch, policy_net, value_net):
     rewards = torch.Tensor(batch.reward)
     masks = torch.Tensor(batch.mask)
     actions = torch.Tensor(np.concatenate(batch.action, 0))
-    states = torch.Tensor(batch.state)
+    states = torch.Tensor(np.array(batch.state)) # cast list to np array
     values = value_net(Variable(states))
 
     returns = torch.Tensor(actions.size(0),1)
@@ -117,7 +106,7 @@ def update_params(batch, policy_net, value_net):
         # action_loss = -Variable(advantages) * torch.exp(log_prob - Variable(fixed_log_prob))
         action_loss1 = Variable(advantages) * torch.exp(log_prob - Variable(fixed_log_prob))
         # print('Ratio: {}'.format(torch.exp(log_prob - Variable(fixed_log_prob))[torch.exp(log_prob - Variable(fixed_log_prob))<0]))
-        eps = 0.2
+        eps = 0.5
         action_loss2 = Variable(advantages) * torch.clip(torch.exp(log_prob - Variable(fixed_log_prob)), min=1-eps, max=1+eps)
         action_loss = -torch.min(action_loss1, action_loss2)
         return action_loss.mean()
@@ -132,10 +121,10 @@ def update_params(batch, policy_net, value_net):
         kl = log_std1 - log_std0 + (std0.pow(2) + (mean0 - mean1).pow(2)) / (2.0 * std1.pow(2)) - 0.5
         return kl.sum(1, keepdim=True)
 
-    if opt =='ARCLSR1':
-        ARCLSR1optimize.arclsr1(policy_net, get_loss, get_kl, args.max_kl, args.damping, environment)
+    if opt =='ARCLSR1': # Our Proposed
+        optimize.arclsr1(policy_net, get_loss, get_kl, args.max_kl, args.damping, environment)
 
-    if opt =='trpo':
+    if opt =='trpo': # Existing Solution
         trpo_step(policy_net, get_loss, get_kl, args.max_kl, args.damping)
 
     if opt == 'InteriorPointMethod':
@@ -143,12 +132,10 @@ def update_params(batch, policy_net, value_net):
 
 
 
-envs = ['HumanoidStandup-v2']
-opts = ['ARCLSR1']
+envs = ['Swimmer-v4'] # Look up other enviornments ,
+opts = ['ARCLSR1', 'trpo'] #
 
-ARCLSR1optimize = ARCLSR1(maxhist = 2, maxiters = 2, verbose=True)
-InteriorOptimize = InteriorPointMethod(maxiters=2, maxhist=2, method='LBFGS')
-
+optimize = ARCLSR1(maxhist = 100, maxiters = 100, verbose=True)
 
 for environment in envs:
     for opt in opts:
@@ -156,9 +143,14 @@ for environment in envs:
         env = gym.make(environment)
 
         num_inputs = env.observation_space.shape[0]
+
+        # set_trace()
+
         num_actions = env.action_space.shape[0]
 
-        env.seed(args.seed)
+        print(num_inputs, num_actions)
+
+        env.reset(seed=args.seed)
         torch.manual_seed(args.seed)
 
         policy_net = Policy(num_inputs, num_actions).to(device)
@@ -169,24 +161,42 @@ for environment in envs:
         running_state = ZFilter((num_inputs,), clip=5)
         running_reward = ZFilter((1,), demean=False, clip=10)
 
-        episodes = 500
+        episodes = 500 # Min 500; Max 5000
 
-        for i_episode in count(1):
+        for i_episode in count(1): # 1, 2, . . . , episodes; Auto-increments, but must break out of loop
+
+            print(f"\nEpisode: {i_episode}")
+
+            # if i_episode == 10:
+                # break
+
             memory = Memory()
 
             num_steps = 0
             reward_batch = 0
             num_episodes = 0
+
+            print(f"batch size: {args.batch_size}")
+            print(f"num_steps: {num_steps}\n")
+
             while num_steps < args.batch_size:
+
                 state = env.reset()
-                state = running_state(state)
+
+                # set_trace()
+
+                state = running_state(state[0]) # 376 states in HumanoidStandup-v4
 
                 reward_sum = 0
+
                 for t in range(10000): # Don't infinite loop while learning
                     action = select_action(state, policy_net, value_net)
+
                     action = action.data[0].cpu().numpy()
+
                     #time.sleep(0.002)
-                    next_state, reward, done, _ = env.step(action)
+                    next_state, reward, done, _, _ = env.step(action)
+
                     reward_sum += reward
 
                     next_state = running_state(next_state)
@@ -203,7 +213,10 @@ for environment in envs:
                         break
 
                     state = next_state
+
+                print(f"reward sum: {reward_sum}\n")
                 num_steps += (t-1)
+                print(num_steps, args.batch_size)
                 num_episodes += 1
                 reward_batch += reward_sum
 
