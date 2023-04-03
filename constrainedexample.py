75867b53bc9fa31e1329cd48a99a6e41df600b1c
Aditya <aranganath@ucmerced.edu>
2023-04-03 12:30:53 -0700

    Working version of the interior-point method

--- constrainedexample.py
+++ constrainedexample.py
@@ -6,22 +6,23 @@ import numpy as np
 import torch
 import pickle as pkl
 import argparse
+from pdb import set_trace
 
 parser = argparse.ArgumentParser()
 
 
 # Torch
 # define the rosenbrock function
-torchrosenbrock = lambda x: torch.sum((torch.ones(x[0].shape) - x[0])**2 + (x[1] - x[0]**2)**2)
+torchrosenbrock = lambda x: 100*(x[1] - x[0]**2)**2 + (1 - x[0])**2
 
 # Define the constraint
-torchradconstraint =  lambda x: x[2]*(delta**2-(x[0]**2 + x[1]**2) - x[3])
+torchradconstraint =  lambda x: (x[0]**2 + x[1]**2 - x[2])
 
-# Define the log-barrier function
-torchbarrier = lambda x: mu*torch.log(x[3])
+# Define log barrier
+torchbarrier = lambda x: torch.log(x[2])
 
 #Define the optimization problem
-torchfunc = lambda x: torchrosenbrock(x) + torchradconstraint(x) - torchbarrier(x)
+torchfunc = lambda x: torchrosenbrock(x) - x[3]*torchradconstraint(x) - mu*torchbarrier(x)
 
 def torchsolution(x):
 	N = torch.autograd.functional.hessian(torchfunc, x)
@@ -29,35 +30,10 @@ def torchsolution(x):
 	sol = - torch.linalg.inv(N) @ grads
 	return sol
 
-
-def torchlinesearch(x, sol, func):
-	# Find the solution to the problem
-	# Check if the linesearch sufficiently reduces the function
-	# Take the step if it does
-	# merit = lambda x: torchrosenbrock(x) - torchbarrier(x)
-	merit = lambda x: torchfunc(x)
-	ls_iters = 10
-	i=0
-	alpha = 1
-	grad = torch.autograd.grad(torchfunc(x), inputs=x)[0]
-	gradsol = torch.autograd.grad(torchfunc(x+alpha*sol), inputs=x)[0]
-	c1 = 0.5
-	c2 = 0.5
-	# from pdb import set_trace
-	# set_trace()
-	while i < ls_iters and torchfunc(x+ alpha*sol)<= torchfunc(x) + c1*alpha*grad.dot(sol) and grad.dot(sol)>=c2*grad.dot(sol):
-		alpha = 0.9*alpha
-		i+=1
-
-	if i < ls_iters:
-		return True, x + alpha*sol
-	else:
-		return False, x
-
 def prettyprint(delta, mu, func_val, start, iteration, flag):
 	print(start.data)
 	
-	print('Iteration:'+str(iteration)+'\t functional value:'+str(torchfunc(start).data)+'\n')
+	print('Iteration:'+str(iteration)+'\t functional value:'+str(func_val)+'\n')
 	print('mu:'+str(mu)+'\t Used line-search:'+str(flag))
 
 
@@ -65,8 +41,8 @@ def prettyprint(delta, mu, func_val, start, iteration, flag):
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--delta', type=float, default=2.0)
-	parser.add_argument('--mu', type=float, default=2.0)
-	parser.add_argument('--max-iters', type=int, default=10)
+	parser.add_argument('--mu', type=float, default=10.0)
+	parser.add_argument('--max-iters', type=int, default=100)
 	parser.add_argument('--line-search-iters', type=int, default=10)
 	parser.add_argument('--save', type=bool, default=False)
 	parser.add_argument('--location', type=str, default='./rosenbrockresults/raw/path.pkl')
@@ -75,15 +51,19 @@ if __name__ == '__main__':
 	max_iters = args.max_iters
 	mu = args.mu
 	iterates = []
-	start = torch.tensor([3.,3.,1e-2,1], requires_grad=True)
-	print(start.data.numpy())
-	iterates.append(start.data.numpy())
+	sol = torch.tensor([10.,10., 2., 1e6], requires_grad=True)
+	print(sol.data.numpy())
+	iterates.append(sol.data.numpy())
 	for i in range(max_iters):
-		sol = torchsolution(start)
-		flag, start = torchlinesearch(start, sol, torchfunc)
-		iterates.append(start.data.numpy())
-		func_val = torchfunc(start).data
-		prettyprint(delta, args.mu, func_val, start, i, flag)
+		delsol = torchsolution(sol).data
+		with torch.no_grad():
+			sol = sol + delsol.data
+		iterates.append(sol.data.numpy())
+		func_val = torchrosenbrock(sol).data
+		flag = False
+		mu = 0.01*mu
+		sol.requires_grad=True
+		prettyprint(delta, mu, func_val, sol, i, flag)
 
 	if args.save:
 		with open(location, 'wb') as file:
